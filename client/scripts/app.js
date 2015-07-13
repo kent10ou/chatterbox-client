787ce11c455e84da628d1bd93a2f3c4f8bebf1fc
kent10ou <kent10ou@gmail.com>
2015-07-13 15:49:50 -0700

    "Finish fetching" not sure if thats the proper way

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -23,18 +23,16 @@ var app = {
 
 	fetch: function () {
 		$.ajax({
-		  // This is the url you should use to communicate with the parse API server.
-		  url: 'https://api.parse.com/1/classes/chatterbox',
-		  type: 'GET',
-		  contentType: 'application/json',
-		  success: function (data) {
-		    console.log('data: ', data);
-		  },
-		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-		    console.error('Failed to send message');
-		  }
+		  url: undefined,
+		  // data: JSON.stringify(),
+		  // success: function (data) {
+		  // 	console.log('sent data: ', data);
+		  // }
 		});
+	},
+
+	clearMessages: function () {
+		
 	}
 
 
